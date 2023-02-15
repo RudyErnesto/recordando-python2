a17393a96dcc14454d467a3ae6cc2b36333e3123
RudyErnesto <32149729+RudyErnesto@users.noreply.github.com>
2023-02-14 23:11:12 -0400

    primera parte

---
+++ 13_modulos.py
@@ -0,0 +1,7 @@
+### modulos ###
+from math import pi as PI_VALUE
+from mi_modulo import sumaValue, printValue
+import mi_modulo
+
+sumaValue(1,2,3)
+printValue(PI_VALUE)
---
+++ mi_modulo.py
@@ -0,0 +1,7 @@
+### archivo de pruebas para imports ###
+
+def sumaValue(numerberOne, numberTwo, numberThree):
+    print(numerberOne + numberTwo+ numberThree)
+
+def printValue(value):
+    print(value)
