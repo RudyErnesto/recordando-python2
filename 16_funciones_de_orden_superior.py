19d4d0f84fafc9cb11904f3fc24b10bd32888901
RudyErnesto <32149729+RudyErnesto@users.noreply.github.com>
2023-02-15 12:32:39 -0400

    m

--- 16_funciones_de_orden_superior.py
+++ 16_funciones_de_orden_superior.py
@@ -30,3 +30,39 @@ def sum_ten(original_value):
 add_closure = sum_ten(1)
 print(add_closure(5))
 print((sum_ten(5))(1))
+
+
+
+
+numbers = [2, 5, 10, 21, 3, 30]
+
+# Map
+
+
+def multiply_two(number):
+    return number * 2
+
+
+print(list(map(multiply_two, numbers)))
+print(list(map(lambda number: number * 2, numbers)))
+
+# Filter
+
+
+def filter_greater_than_ten(number):
+    if number > 10:
+        return True
+    return False
+
+
+print(list(filter(filter_greater_than_ten, numbers)))
+print(list(filter(lambda number: number > 10, numbers)))
+
+# Reduce
+
+
+def sum_two_values(first_value, second_value):
+    return first_value + second_value
+
+
+print(reduce(sum_two_values, numbers))
